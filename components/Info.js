421745b6dfd4c5fea762e9d2512fb875f8a8fb3f
yummygoods <melissa@yummygoods.com>
2023-05-30 19:16:09 -0400

    change name

--- components/Info.js
+++ components/Info.js
@@ -1,17 +1,16 @@
-import React from "react";
-   
-   
-   export default function Info(){
-    return (
-        <div className="info">
-        <img src="https://avatars.githubusercontent.com/u/105559874?v=4" />
-        <h1 className="name">name</h1>
-        <h3 className="job">words</h3>
-        <h4 className="site">smaller words</h4>
-   <a href=""> <button>email</button> </a> 
- 
-        </div>
-        
-    );
+import React from 'react';
+
+export default function Info() {
+	return (
+		<div className="info">
+			<img src="https://avatars.githubusercontent.com/u/105559874?v=4" />
+			<h1 className="name">name</h1>
+			<h3 className="job">words</h3>
+			<h4 className="site">smaller words</h4>
+			<a href="">
+				{' '}
+				<button>email</button>{' '}
+			</a>
+		</div>
+	);
 }
-   
